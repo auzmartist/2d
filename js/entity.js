5e48201a1c32b17ffef6bc850421642eb6947fd1
auzmartist <auz.martin@gmail.com>
2014-08-18 21:58:25 -0700

    Added tut

--- js/entity.js
+++ js/entity.js
@@ -4,22 +4,11 @@
 // the resulting collision calculations
 
 var Collision = {
-
-    // Elastic collisions refer to the simple cast where
-    // two entities collide and a transfer of energy is
-    // performed to calculate the resulting speed
-    // We will follow Box2D's example of using
-    // restitution to represent "bounciness"
-
     elastic: function(restitution) {
         this.restitution = restitution || .2;
     },
 
     displace: function() {
-        // While not supported in this engine
-	       // the displacement collisions could include
-        // friction to slow down entities as they slide
-        // across the colliding entity
     }
 };
 
@@ -122,3 +111,121 @@ PhysicsEntity.DISPLACE = 'displace';
 // the colliding entity off by reducing the velocity by
 // its restituion coefficient
 PhysicsEntity.ELASTIC = 'elastic';
+
+//COLLISION DETECTION
+CollisionDetector.prototype.collideRect = function(collider, collidee){
+    var l1 = collider.getLeft();
+    var t1 = collider.getTop();
+    var r1 = collider.getRight();
+    var b1 = collider.getBottom();
+
+    var l2 = collidee.getLeft();
+    var t2 = collidee.getTop();
+    var r2 = collidee.getRight();
+    var b2 = collidee.getBottom();
+
+    if( b1 < t2 || t1 > b2 || r1 < l2 || l1 > r2){
+        return false;
+    }
+    return true;
+}
+
+//COLLISION RESOLUTION
+resolveElastic: function(player, entity){
+    var pMidX = player.getMidX();
+    var pMidX = player.getMidY();
+    var aMidX = entity.getMidX();
+    var aMidX = entity.getMidY();
+
+    var dx = (aMidX - pMidX) / entity.halfWidth;
+    var dy = (aMidY - pMidY) / entity.halfHeight;
+
+    var absDX = abs(dx);
+    var absDY = abs(dy);
+
+    //aproaching from the corner
+    if(abs(absDX - absDY) < 0.1){
+        if(dx < 0){
+            player.x = entity.getRight();
+        }else{
+            player.x = entity.getLeft() - player.width;
+        }
+        if(dy < 0){
+            player.y = entity.getBottom();
+        }else{
+            player.y = entity.getTop() - player.height;
+        }
+
+        if(Math.random() < 0.5){
+            player.vx = -player.vx * entity.restituion;
+
+            if(abs(player.vx) < STICKY_THRESHOLD){
+                player.vx = 0;
+            }
+        }else{
+            player.vy = -player.vy * entity.restituion;
+            if(abs(player.vy) <  STICKY_THRESHOLD){
+                player.vy = 0;
+            }
+        }
+    //collision from side
+    }else if(absDX > absDY){
+        if(dx < 0){
+            player.x = entity.getRight();
+        }else{
+            player.x = entity.getLeft() - player.width;
+        }
+
+        player.vx = -player.vx * entity.restituion;
+
+        if(abs(player.vx) < STICKY_THRESHOLD){
+            player.vx = 0;
+        }
+    //collision from top or bottom
+    }else{
+        if(dy < 0){
+            player.y = entity.getBottom();
+        }else{
+            player.y = entity.getTop() - player.height();
+        }
+
+        player.vy = -player.vy * entity.restituion;
+        if(abs(player.vy) < STICKY_THRESHOLD){
+            player.vy = 0;
+        }
+    }
+};
+
+//ENGINE
+Engine.prototype.step = function(elapsed) {
+    var gx = GRAVITY_X * elapsed;
+    var gy = GRAVITY_Y * elapsed;
+    var entity;
+    var entities = this.entities;
+
+    for(var i = 0, length = entities.length; i < length; i++){
+        entity = entities[i];
+        switch (entity.type){
+            case PhysicsEntity.DYNAMIC:
+                entity.vx += entity.ax * elapsed + gx;
+                entity.vy += entity.ay * elapsed + gy;
+                entity.x += entity.vx * elapsed;
+                entity.y += entity.vy * elapsed;
+                break;
+            case PhysicsEntity.KINEMATIC:
+                entity.vx += entity.ax * elapsed;
+                entity.vy += entity.ay * elapsed;
+                entity.x += entity.vx * elapsed;
+                entity.y += entity.vy * elapsed;
+                break;
+        }
+    }
+
+    var collisions = this.collider.detectCollisions(this.player, this.collidables);
+
+    if (collisions != null){
+        this.solver.resolve(this.player, collisions);
+    }
+};
+
+
